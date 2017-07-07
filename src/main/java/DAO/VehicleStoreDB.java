e9be049ca06d4b1b0c03db40be66ba0d168ebafa
Hemil Parmar <hemilcp@gmail.com>
2017-07-07 03:57:05 -0700

    Added models

--- src/main/java/DAO/VehicleStoreDB.java
+++ src/main/java/DAO/VehicleStoreDB.java
@@ -3,17 +3,45 @@ package DAO;
 import java.util.ArrayList;
 import java.util.List;
 
-import models.vehicle;
+import javax.ws.rs.core.Response;
+
+import models.Vehicle;
 
 public class VehicleStoreDB {
 
-	static List<vehicle> vehicledb = new ArrayList<>();
+	static List<Vehicle> vehicledb = new ArrayList<>();
+	static int Id_count = 1;
 	
 	public VehicleStoreDB(){
-		
+//		Vehicle v1 = new Vehicle(0,2010,"Tesla","Model S");
+//		vehicledb.add(v1);
 	}
 	
-	public List<vehicle> getAllVehicleList(){
+	public List<Vehicle> getAllVehicleList(){
 		return this.vehicledb;
 	}
+	
+	public boolean addVehicleDetails(Vehicle input){
+		if(!checkIfVehicleExists(input)){
+			input.id = Id_count;
+			Id_count++;
+			vehicledb.add(input);
+			return true;
+		}
+		return false;
+	}
+
+	private boolean checkIfVehicleExists(Vehicle input) {
+		for(int i = 0; i < vehicledb.size(); i++){
+			if(vehicledb.get(i).getMake() == input.getMake() && 
+					vehicledb.get(i).getModel() == input.getModel()
+					&& vehicledb.get(i).getYear() == input.getYear()){
+				return true;
+			}
+					
+		}
+		return false;
+	}
+	
+	
 }
